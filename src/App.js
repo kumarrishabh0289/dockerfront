a2990b0509b1a23a5e85e35b03558c11235589bc
eCampus Guest <ecampusguest@eCampus-M11.local>
2020-04-14 07:58:28 -0700

    changed app.js

--- src/App.js
+++ src/App.js
@@ -8,9 +8,16 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Rishabh The Great you know. HELLO
+          Edit <code>src/App.js</code> and save to reload.
         </p>
-       
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
     </div>
   );
